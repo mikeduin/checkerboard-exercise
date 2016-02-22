2d5dcc330cc9eee72687b94fbccced4d52828d68
Mike Duin <michael.s.duin@gmail.com>
2016-02-22 01:12:14 -0800

    random color solution

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 // Your JS goes here
 
-for (var i = 0; i<27; i++) {
-  var blackTile = document.createElement("div");
+for (var i = 0; i<54; i++) {
+  var tile = document.createElement("div");
   var body = document.getElementsByTagName("body")[0];
-  body.appendChild(blackTile);
-  blackTile.style.backgroundColor = "black";
-  blackTile.style.width = "11.1%";
-  blackTile.style.float = "left";
-  blackTile.style.paddingBottom = "11.1%";
+  body.appendChild(tile);
 
-  var redTile = document.createElement("div");
-  var body = document.getElementsByTagName("body")[0];
-  body.appendChild(redTile);
-  redTile.style.backgroundColor = "red";
-  redTile.style.width = "11.1%";
-  redTile.style.float = "left";
-  redTile.style.paddingBottom = "11.1%";
+  var r = Math.floor(Math.random() * 255);
+  var g = Math.floor(Math.random() * 255);
+  var b = Math.floor(Math.random() * 255);
+  var randColor = "rgb(" + r + ", " + g + ", " + b + ")"
+
+  tile.style.backgroundColor = randColor
+
+  tile.style.width = "11.1%";
+  tile.style.float = "left";
+  tile.style.paddingBottom = "11.1%";
 }
