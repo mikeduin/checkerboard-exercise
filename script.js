4c5ebbaee5f7dd8c0ceb35c7df80ed8e80113b8a
Mike Duin <michael.s.duin@gmail.com>
2016-02-22 01:37:09 -0800

    gradient solution

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-// Your JS goes here
+for (var i = 0; i<27; i++) {
 
-for (var i = 0; i<54; i++) {
-  var tile = document.createElement("div");
+  var r = (100);
+  var g = (100);
+  var b = (100);
+  var aTileForm = "rgb(" + (r+(i*2)) + ", " + g + ", " + b + ")"
+  var bTileForm = "rgb(" + (r-(i*2)) + ", " + g + ", " + b + ")"
+
+  var aTile = document.createElement("div");
   var body = document.getElementsByTagName("body")[0];
-  body.appendChild(tile);
+  body.appendChild(aTile);
+  aTile.style.backgroundColor = aTileForm;
+  aTile.style.width = "11.1%";
+  aTile.style.float = "left";
+  aTile.style.paddingBottom = "11.1%";
 
-  var r = Math.floor(Math.random() * 255);
-  var g = Math.floor(Math.random() * 255);
-  var b = Math.floor(Math.random() * 255);
-  var randColor = "rgb(" + r + ", " + g + ", " + b + ")"
-
-  tile.style.backgroundColor = randColor
-
-  tile.style.width = "11.1%";
-  tile.style.float = "left";
-  tile.style.paddingBottom = "11.1%";
+  var bTile = document.createElement("div");
+  var body = document.getElementsByTagName("body")[0];
+  body.appendChild(bTile);
+  bTile.style.backgroundColor = bTileForm;
+  bTile.style.width = "11.1%";
+  bTile.style.float = "left";
+  bTile.style.paddingBottom = "11.1%";
 }
